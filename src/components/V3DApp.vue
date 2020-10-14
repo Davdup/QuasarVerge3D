9b592d9a33f69d0ab01c0c59db1985d97bccc905
David Duperron <dav_dup@yahoo.fr>
2020-10-14 13:55:13 +0200

    Initial upload
    Initial upload of the app

---
+++ src/components/V3DApp.vue
@@ -0,0 +1,55 @@
+<template>
+  <div :id="containerId">
+    <q-btn
+      color="white"
+      text-color="black"
+      label="Dialog"
+      class="fixed-bottom-right q-ma-md"
+      @click="alert=true"
+    />
+    <q-dialog v-model="alert">
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">Alert</div>
+        </q-card-section>
+
+        <q-card-section
+          class="q-pt-none"
+        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.</q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="OK" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </div>
+</template>
+
+<script>
+import * as V3DApp from "../v3dApp/app.js";
+import * as LOGIC from "../v3dApp/visual_logic.js";
+
+export default {
+  name: "V3DApp",
+
+  data() {
+    return {
+      containerId: V3DApp.CONTAINER_ID,
+      alert: LOGIC.alert2,
+      test: LOGIC.testVar,
+    };
+  },
+
+
+  mounted: function () {
+    V3DApp.createApp();
+    //V3DApp;
+    console.log("test :", this.test);
+    console.log("alert :", alert);
+  },
+};
+</script>
+
+<style>
+@import "../v3dApp/app.css";
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <V3DApp></V3DApp>
+  </div>
+</template>
+
+<script>
+import V3DApp from '../components/V3DApp.vue';
+
+export default {
+  name: 'App',
+  components: {
+    V3DApp
+  }
+}
+</script>
